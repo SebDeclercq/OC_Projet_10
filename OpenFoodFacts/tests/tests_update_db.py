0ba17029306f396eaa5971bc4eb7f37fe7e3a420
declercq sebastien <sdq@afnor.fr>
2019-05-29 12:37:23 +0200

    OFF: Update DB: first test

--- OpenFoodFacts/tests/tests_update_db.py
+++ OpenFoodFacts/tests/tests_update_db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from django.core.management import call_command
 from django.test import TestCase
+from OpenFoodFacts.update_db import FoodDbUpdater
 
 
 class TestUpdateCommand(TestCase):
@@ -10,3 +11,14 @@ class TestUpdateCommand(TestCase):
         except Exception as e:
             self.fail('The command "update_food_db"'
                       f' raised {e.__class__.__name__} unexpectedly')
+
+
+class TestFoodDbUpdater(TestCase):
+    def setUp(self) -> None:
+        self.db_updater: FoodDbUpdater = FoodDbUpdater()
+
+    def test_init(self) -> None:
+        db_updater: FoodDbUpdater = FoodDbUpdater(csv_separator=';')
+        self.assertIn('openfoodfacts.org', db_updater.off_csv_url)
+        self.assertEqual(';', db_updater.csv_separator)
+        self.assertTrue(hasattr(db_updater, 'off_csv_file'))
--- OpenFoodFacts/update_db.py
+++ OpenFoodFacts/update_db.py
@@ -1,6 +1,19 @@
+#!/usr/bin/env python3
+from dataclasses import dataclass
+import os
+import tempfile
+
+
+@dataclass
 class FoodDbUpdater:
-    def __init__(self) -> None:
-        ...
+    off_csv_url: str = 'https://fr.openfoodfacts.org/data/fr.openfoodfacts.org.products.csv'  # noqa
+    csv_separator: str = '\t'
+    tmp_dir: str = tempfile.gettempdir()
+    output_file: str = 'off.products.csv'
+
+    @property
+    def off_csv_file(self) -> str:
+        return os.path.join(self.tmp_dir, self.output_file)
 
     def run(self) -> None:
         ...
