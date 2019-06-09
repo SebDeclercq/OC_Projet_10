c3ebf239519a85ce6668b31f2d43db92385aaca6
declercq sebastien <sdq@afnor.fr>
2019-06-09 19:22:01 +0200

    OFF: Docstrings for update_db.py

--- OpenFoodFacts/update_db.py
+++ OpenFoodFacts/update_db.py
@@ -10,6 +10,7 @@ from Food.models import Product
 
 @dataclass
 class CsvData:
+    '''Data class holding the information collected in the OFF CSV file'''
     code: str
     product_name: str
     nutrition_grade_fr: str
@@ -19,6 +20,7 @@ class CsvData:
 
 @dataclass
 class FoodDbUpdater:
+    '''Main class used to update the food database'''
     off_csv_url: str = 'https://fr.openfoodfacts.org/data/fr.openfoodfacts.org.products.csv'  # noqa
     csv_separator: str = '\t'
     tmp_dir: str = tempfile.gettempdir()
@@ -31,15 +33,19 @@ class FoodDbUpdater:
 
     @property
     def off_csv_file(self) -> str:
+        '''The absolute path to the temporary csv file downloaded from OFF'''
         return os.path.join(self.tmp_dir, self.output_file)
 
     def run(self) -> None:
+        '''Main method'''
         self.get_off_csv_file()
         self.get_products()
         for (product, csv_data) in self.get_products_data():
             self.update_product(product, csv_data)
 
     def get_off_csv_file(self) -> None:
+        '''Download the OFF CSV file by chunks
+        (in order to consume less memory)'''
         try:
             with requests.get(self.off_csv_url, stream=True) as resp:
                 resp.raise_for_status()
@@ -51,11 +57,15 @@ class FoodDbUpdater:
             raise FileNotFoundError(f'{self.off_csv_url} is unavailable')
 
     def get_products(self) -> Dict[str, Product]:
+        '''Collect all products from the Food database and set up a dict with
+        Product.barcodes as keys and Product objects as values'''
         for product in Product.objects.all():
             self.products[product.barcode] = product
         return self.products
 
     def get_products_data(self) -> Iterator[Tuple[Product, CsvData]]:
+        '''Generator iterating on the OFF CSV File and matching the
+        useful metadata CsvData and Product objects for updates'''
         with open(self.off_csv_file) as off_file:
             off_data: Any = csv.DictReader(
                 off_file, delimiter=self.csv_separator
@@ -67,6 +77,7 @@ class FoodDbUpdater:
                            CsvData(**self._extract_useful_data(raw_data)))
 
     def _extract_useful_data(self, data: Dict[str, Any]) -> Dict[str, Any]:
+        '''Extract useful data from the raw data collected by DictReader'''
         useful_keys: Set[str] = {f.name for f in fields(CsvData)}
         result: Dict[str, Any] = {}
         for key, val in data.items():
@@ -75,6 +86,7 @@ class FoodDbUpdater:
         return result
 
     def update_product(self, product: Product, data: CsvData) -> None:
+        '''Update the Product in the database w/ the new CsvData'''
         for csv_attr, db_attr in self.matching_csv_db.items():
             setattr(product, db_attr, getattr(data, csv_attr))
             product.save()
